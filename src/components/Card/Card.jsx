a2455b1c3c498c7706c538caa364f2a5c4f19cd3
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-05-16 12:45:31 +0200

    use of objects from db.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,14 @@
 import './card.css'
 
-export default function Card({ title, description, image, stars }) {
+export default function Card({ nombre, descripcion, imagenes, stars = 4.2 }) {
+  console.log(descripcion)
   return (
     <div className="card">
       <div className="card-image">
         <picture>
           <img
             style={{ objectFit: 'cover', verticalAlign: 'bottom' }}
-            src={image}
+            src={imagenes[0]}
             alt="Card Image"
           />
         </picture>
@@ -15,16 +16,53 @@ export default function Card({ title, description, image, stars }) {
       <div className="card-content">
         <div className="card-header">
           <div className="card-title">
-            <span className="title-4">{title} </span>
+            <span className="title-4">{nombre} </span>
             <span className="subtitle-4">¡Disponible!</span>
           </div>
           <div className="card-rating">
-            <span>{stars}/5</span>
+            <span className="rating">{stars}/5</span>
             <span className="star"></span>
           </div>
         </div>
 
-        <span className="text">{description}</span>
+        <div className="card-description">
+          <span className="item">
+            Vistas al mar:{' '}
+            {descripcion.vistas_playa ? (
+              <span className="check-green"></span>
+            ) : (
+              <span className="check-red">X</span>
+            )}
+          </span>
+          <span className="item">
+            Baño:{' '}
+            {descripcion.baño ? (
+              <span className="check-green"></span>
+            ) : (
+              <span className="check-red"></span>
+            )}
+          </span>
+          <span className="item">
+            Television:{' '}
+            {descripcion.television ? (
+              <span className="check-green"></span>
+            ) : (
+              <span className="check-red"></span>
+            )}
+          </span>
+          <span className="item">
+            Telefono:{' '}
+            {descripcion.telefono ? (
+              <span className="check-green"></span>
+            ) : (
+              <span className="check-red"></span>
+            )}
+          </span>
+        </div>
+
+        <div className="booking">
+          <button>¡RESERVAR AHORA!</button>
+        </div>
       </div>
     </div>
   )
