a158a7fae706e7293592b2be7148fe498630b670
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-05-20 11:17:44 +0200

    use of context and new booking route

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { ContextWrapper } from './contexts/dateContext'
 import App from './pages/App'
 import Prueba from './pages/Prueba'
+import BookingRoom from './pages/booking/room'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/prueba" element={<Prueba />} />
-    </Routes>
+    <ContextWrapper>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/prueba" element={<Prueba />} />
+        <Route path="/booking/:id" element={<BookingRoom />} />
+      </Routes>
+    </ContextWrapper>
   </Router>
 )
