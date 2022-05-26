bea3beeec47697aa62f2671041f1463257323225
Diego Garcia Navas <diegogarcianavas.4b@gmail.com>
2022-05-26 14:23:50 +0200

    use of zipper to compress text as performance strategy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'brotli',
+      threshold: 10240,
+      minRatio: 0.8,
+      compressionOptions: {
+        level: 11,
+      },
+    }),
+  ],
 })
