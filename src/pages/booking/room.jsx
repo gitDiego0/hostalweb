4fa61ecedeceff4b392befb6a5576face2e02dba
Diego Garcia Navas <diegogarcianavas.4b@gmail.com>
2022-05-26 14:22:31 +0200

    use of basic validation and default values

--- src/pages/booking/room.jsx
+++ src/pages/booking/room.jsx
@@ -1,13 +1,14 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useLocation } from 'react-router-dom'
 import { useContext } from 'react'
 import { DateContext } from '../../contexts/dateContext'
 import { useForm } from 'react-hook-form'
 import Layout from '../../components/layout'
+import { getDays } from '../../utils/getDate'
 
 export default function RoomBooking() {
   const { dateIn, dateOut } = useContext(DateContext)
   const { id } = useParams()
-  console.log(id)
+  const { state } = useLocation()
   const {
     register,
     handleSubmit,
@@ -15,20 +16,45 @@ export default function RoomBooking() {
     formState: { errors },
   } = useForm()
   const onSubmit = (data) => console.log(data)
+  const precioFinal = getDays(dateIn, dateOut) * state.precio
   return (
     <Layout>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input placeholder="Nombre" {...register('name')} />
-        <input placeholder="Apellidos" {...register('surname')} />
-        <input placeholder="Email" {...register('email')} />
-        <input placeholder="Telefono" {...register('phone')} />
-        <input placeholder="DNI" {...register('dni')} />
-        <input defaultValue={dateIn} {...register('dateIn')} />
-        <input defaultValue={dateOut} {...register('dateOut')} />
-        <input placeholder="Cantidad de personas" {...register('people')} />
-        <input placeholder="Comentarios" {...register('comments')} />
+        <input placeholder="Nombre" {...register('name', { required: true })} />
+        <input
+          placeholder="Apellidos"
+          {...register('surname', { required: true })}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          {...register('email', { required: true })}
+        />
+        <input
+          type="phone"
+          placeholder="Telefono"
+          {...register('phone', { required: true })}
+        />
+        <input
+          placeholder="DNI/NIE/PASAPORTE"
+          {...register('dni', { required: true })}
+        />
+        <input defaultValue={dateIn} {...register('dateIn')} disabled />
+        <input defaultValue={dateOut} {...register('dateOut')} disabled />
+        <input
+          type="number"
+          max={state.personas_max}
+          min={state.personas_min}
+          placeholder="Cantidad de personas"
+          {...register('people', {
+            validate: (value) =>
+              value >= state.personas_min && value <= state.personas_max,
+          })}
+        />
+        <textarea placeholder="Comentarios" {...register('comments')} />
         <input type="submit" />
       </form>
+      <span>{precioFinal}</span>
     </Layout>
   )
 }
