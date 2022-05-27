ebebd4e63d40ffea198c2357b0d2983dbfba9b5d
Diego Garcia Navas <diegogarcianavas.4b@gmail.com>
2022-05-27 13:21:31 +0200

    patterns and error handlers to validate form

--- src/pages/booking/room.jsx
+++ src/pages/booking/room.jsx
@@ -4,6 +4,8 @@ import { DateContext } from '../../contexts/dateContext'
 import { useForm } from 'react-hook-form'
 import Layout from '../../components/layout'
 import { getDays } from '../../utils/getDate'
+import { checkDNI } from '../../utils/checkID'
+import Text from '../../components/Text/Text'
 
 export default function RoomBooking() {
   const { dateIn, dateOut } = useContext(DateContext)
@@ -21,26 +23,47 @@ export default function RoomBooking() {
     <Layout>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input placeholder="Nombre" {...register('name', { required: true })} />
+
         <input
           placeholder="Apellidos"
           {...register('surname', { required: true })}
         />
+
         <input
           type="email"
           placeholder="Email"
           {...register('email', { required: true })}
         />
+
+        {errors.phone && (
+          <Text as="span" color="red" fontSize="12px" align="left">
+            El telefono debe tener 9 digitos
+          </Text>
+        )}
         <input
-          type="phone"
+          type="tel"
           placeholder="Telefono"
-          {...register('phone', { required: true })}
+          {...register('phone', { required: true, pattern: /^\d{9}$/ })}
         />
+
+        {errors.dni && (
+          <Text as="span" color="red" fontSize="12px" align="left">
+            Numero de identificación incorrecto
+          </Text>
+        )}
         <input
           placeholder="DNI/NIE/PASAPORTE"
-          {...register('dni', { required: true })}
+          {...register('dni', {
+            required: true,
+            pattern:
+              /^[0-9]{8}[A-Z]$/i | /^[A-Za-z][A-Za-z0-9]{7,8}[^A-Za-z0-9]$/i,
+          })}
         />
+
         <input defaultValue={dateIn} {...register('dateIn')} disabled />
+
         <input defaultValue={dateOut} {...register('dateOut')} disabled />
+
         <input
           type="number"
           max={state.personas_max}
@@ -52,9 +75,12 @@ export default function RoomBooking() {
           })}
         />
         <textarea placeholder="Comentarios" {...register('comments')} />
+        <div className="container">
+          <Text as="h3">PRECIO FINAL: </Text>
+          <Text as="span">{precioFinal}€</Text>
+        </div>
         <input type="submit" />
       </form>
-      <span>{precioFinal}</span>
     </Layout>
   )
 }
